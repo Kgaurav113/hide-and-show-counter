eda579a210e15f6e27accac3e876ce98b6013ac4
Kumar Gaurav <kgaurav501@gmail.com>
2022-04-15 04:30:25 +0530

    first commit

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,23 +1,25 @@
-import React,{useState ,useRef} from 'react';
+import React, { useState, useRef } from "react";
 
 export default function Counter() {
-    const[Count,Setcount]= useState(0);
-    
-    let x=0;
-    console.log(x)
+  const [Count, Setcount] = useState(0);
+
+  let x = 0;
+  console.log(x);
   return (
-    
     <div>
-
-
-<h1>Counter:{Count}</h1>
-<div>
-<button onClick={()=>{Setcount(Count+1); x=x+1;
-console.log("new x",x);}}>Increment</button>
-<button onClick={()=>Setcount(Count-1)}>Decrement</button>
-
-</div>
-
+      <h1>Counter:{Count}</h1>
+      <div>
+        <button
+          onClick={() => {
+            Setcount(Count + 1);
+            x = x + 1;
+            console.log("new x", x);
+          }}
+        >
+          Increment
+        </button>
+        <button onClick={() => Setcount(Count - 1)}>Decrement</button>
+      </div>
     </div>
   );
 }
